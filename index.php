57b77f95483818434dd7ea9f9564ef046f690d53
skydeamon <skycharmingdeamon@gmail.com>
2021-01-28 06:00:58 +0200

    Tab functionality works

---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<!-- CSS ================================================== -->
+	<link rel="stylesheet" href="./css/main.css">
+</head>
+<body class="body">
+	<div class="banner"></div>
+
+	<!-- Tab links -->
+	<script type="module" type="text/javascript" src="./js/app.js"></script>
+	<div class="tab">
+		<button data-id="Home" class="tablinks">Home</button>
+		<button data-id="Projects" class="tablinks">Projects</button>
+		<button data-id="Skills" class="tablinks">Skills</button>
+		<button data-id="Interests" class="tablinks">Interests</button>
+		<button data-id="Experience" class="tablinks">Experience</button>
+		<button data-id="Education" class="tablinks">Education</button>
+	</div>
+
+	<!-- Tab content -->
+	<section id="Home" class="tabcontent">
+		<div class="home-banner">
+			<div class="profile-pic">
+			</div>
+			<div class="slider-1"></div>
+			<div class="slider-2"></div>
+			<div class="slider-3"></div>
+		</div>
+		<div class="home-content">
+			<ul>
+				<li>
+					<h4>About me:</h4>
+					<p>
+						I am an aspiring
+						Astrophysicist, Physicist and
+						Computer Scientist. I love
+						solving problems and I have
+						come across more questions
+						than answers.
+						I am an ardent problem
+						solver and my work in the
+						past has taught me soft skills
+						such as good
+						communication, handling
+						events, patience in the
+						working environment and
+						how to even handle the
+						working environment
+						generally.
+					</p>
+				</li>
+				<li>
+					<h4>Career Goals:</h4>
+					<p>
+						The discipline I have chosen
+						to follow is certainly a tough
+						one, regardless my goal is to
+						be able to publicize the ideas
+						that are at the forefront of
+						science. I also aim to change
+						people’s lives through the
+						technology they use, for
+						access to information and
+						ability to advance societies
+						to better life conditions.
+					</p>
+				</li>
+			</ul>
+		</div>
+	</section>
+
+	<section id="Projects" class="tabcontent">
+		<div class="tab-banner"></div>
+		<div class="content">
+		</div>
+	</section>
+
+	<section id="Skills" class="tabcontent">
+		<div class="tab-banner"></div>
+		<div class="content">
+			<ul>
+				<li>
+					<h4>Basic in;</h4>
+					<p>
+						Robotics, OpenOffice, Kotlin.<br>
+						Computer hardware and Support.
+					</p>
+				</li>
+				<li>
+					<h4>Intermediate in;</h4>
+					<p>
+						Physics, Astronomy, Computer Science, Microsoft Office, Matlab, MySQL, PHP, Mathematica, Latex, Linux.
+					</p>
+				</li>
+				<li>
+					<h4>Advanced in;</h4>
+					<p>
+						Mathematics, Python, Java, JavaScript, HTML, CSS, PHP.
+					</p>
+				</li>
+			</ul>
+		</div>
+	</section>
+
+	<section id="Interests" class="tabcontent">
+		<div class="tab-banner">Interests</div>
+		<div class="content">
+			<ul>
+			</ul>
+		</div>
+	</section>
+
+	<section id="Experience" class="tabcontent">
+		<div class="tab-banner"></div>
+		<div class="content">
+			<ul>
+				<li>
+					<h4>Direct Marketing, SWAT </h4>
+					<h5>Marketing representative</h5>
+					<p>
+						Cape Town, Western Cape, South Africa<br>
+						1-Feb-2018 to 30-June-2018
+					</p>
+				</li>
+				<li>
+					<h4>Department of Education, South Africa</h4>
+					<h5>Marking assistant</h5>
+					<p>
+						Tzaneen, Limpopo, South Africa<br>
+						2016-December
+					</p>
+				</li>
+			</ul>
+		</div>
+	</section>
+
+	<section id="Education" class="tabcontent">
+	  	<div class="tab-banner"></div>
+	  	<div class="content">
+		  	<ul>
+		  		<li>
+		  			<h4>University of Cape Town</h4>
+		  			<p>
+		  				Rondebosch, Cape Town Central, ZA 7700<br>
+						2015-2017: incomplete<br>
+						Physics and Astrophysics.
+		  			</p>
+				</li>
+		  		<li>
+		  			<h4>Phusela High School</h4>
+					<p>
+						Lenyenye, Limpopo, South Africa<br>
+						Matriculation 2014<br>
+						Specialized in science subjects.
+					</p>
+		  		</li>
+		  	</ul>
+	  	</div>
+	</section>
+</body>
+</html>
--- js/app.js
+++ js/app.js
@@ -1,19 +1,40 @@
-function openCity(evt, sectName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
+var tablinks = document.querySelectorAll(".tablinks");
+var tabcontent = document.querySelectorAll(".tabcontent");
 
+displayContentNone();
+document.getElementById("Home").style.display = "block";
+
+tablinks.forEach(function(btn){
+    btn.addEventListener("click",function(e){
+        stopDefault(e);
+        displayContentNone();
+        deactivateLink();
+        const section = e.currentTarget.dataset.id;
+        console.log(section);
+        showTab(e,section);
+
+    });
+});
+
+function stopDefault(e){
+    e.preventDefault();
+}
+
+function displayContentNone(){
     // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
+    for(let i = 0; i < tabcontent.length; i++){
         tabcontent[i].style.display = "none";
     }
+}
 
+function deactivateLink(){
     // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
+    for (let i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+}
 
+function showTab(evt, sectName){
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(sectName).style.display = "block";
     evt.currentTarget.className += " active";
